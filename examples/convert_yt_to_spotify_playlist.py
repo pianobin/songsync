b836bf8bfb2d21afab6fb36e87669e525e0f656f
pianobin <shoji@pianobin.com>
2023-12-02 13:28:17 -0800

    TODO

--- examples/convert_yt_to_spotify_playlist.py
+++ examples/convert_yt_to_spotify_playlist.py
@@ -2,6 +2,7 @@
 import sys
 import os
 
+# TODO: Remove this when package is published
 # Add the root directory to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
